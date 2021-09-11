0d1ccc56ef749ae835e78f86542c2bdad2afb4ce
Felipe <luis.delmondes@outlook.com>
2021-09-11 00:59:26 -0300

    commit

---
+++ excessoes e testes/aula1.py
@@ -0,0 +1,38 @@
+opcoes = {'1':lambda x,y: x+y,
+          '2':lambda x,y: x-y,
+          '3':lambda x,y: x*y,
+          '4':lambda x,y: x/y,
+          '5':lambda x,y: exit()
+          }
+
+
+
+while True:
+        
+        try:
+            n1 = float(input('Digite o n1: '))
+            n2 = float(input('Digite o n2: '))
+
+            op = input(f'Digite a opção desejada: \n' \
+                   f'1 - Soma\n' \
+                   f'2 - Subtração\n' \
+                   f'3 - Multiplicação\n' \
+                   f'4 - Divisão\n' \
+                   f'5 - sair \n' \
+                   f'Escolha: ')
+
+            res = opcoes[op](n1,n2)
+        except ZeroDivisionError:
+            print('Não é possível dividir um número por zero')
+
+        except KeyError:
+            print('Valor opção invalida')
+
+        except Exception as err:
+            print('Favor digitar um número', err)
+
+        else:
+            print(res)
+
+        finally:
+            print('Passei por aqui')
